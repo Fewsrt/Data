5b017bec5abf481daae29ab85e137d37ad7e1f9a
FaSAC-Auto <chonnaphat.v@gmail.com>
2020-10-28 23:13:00 +0700

    up

--- Datafinal.java
+++ Datafinal.java
@@ -4,10 +4,92 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Datafinal {
-    static void PrintStack(Stack<String> ops) {
+
+    public Double evaluate(String expression) {
+        Stack<Character> ops = new Stack<>();
+        Queue<Double> numbers = new LinkedList<>();
+        for (int i = 0; i < expression.length(); i++) {
+            char c = expression.charAt(i);
+            if (Character.isDigit(c)) {
+                int num = 0;
+                while (Character.isDigit(c)) {
+                    num = num * 10 + (c - '0');
+                    i++;
+                    if (i < expression.length())
+                        c = expression.charAt(i);
+                    else
+                        break;
+                }
+                i--;
+                numbers.add((double) num);
+            } else if (c == '(') {
+                ops.push(c);
+            } else if (c == ')') {
+                while (ops.peek() != '(') {
+                    double output = performOperation(numbers, ops);
+                    numbers.add((double) output);
+                }
+                ops.pop();
+            } else if (isOperator(c)) {
+                while (!ops.isEmpty() && precedence(c) <= precedence(ops.peek())) {
+                    double output = performOperation(numbers, ops);
+                    numbers.add((double) output);
+                }
+                ops.push(c);
+            }
+        }
+        while (!ops.isEmpty()) {
+            double output = performOperation(numbers, ops);
+            numbers.add((double) output);
+        }
+        return numbers.poll();
+    }
+
+    public Character postfix(Character tokens){
+        return tokens;
+    }
+
+    public double performOperation(Queue<Double> numbers, Stack<Character> ops) {
+        Double a = numbers.poll();
+        Double b = numbers.poll();
+        char checkops = ops.pop();
+        switch (checkops) {
+            case '+':
+                return a + b;
+            case '-':
+                return b - a;
+            case '*':
+                return a * b;
+            case '/':
+                if (a == 0)
+                    throw new UnsupportedOperationException("Cannot divide by zero");
+                return b / a;
+        }
+        return 0;
+    }
+
+    public boolean isOperator(char c) {
+        return (c == '+' || c == '-' || c == '/' || c == '*' || c == '^');
+    }
+
+    private static int precedence(char c) {
+        switch (c) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            case '^':
+                return 3;
+        }
+        return -1;
+    }
+
+    public static void PrintStack(Stack<Character> ops) {
         if (ops.isEmpty())
             return;
-        String x = ops.peek();
+        Character x = ops.peek();
         ops.pop();
         PrintStack(ops);
         System.out.print(x + " ");
@@ -15,39 +97,26 @@ public class Datafinal {
     }
 
     public static void main(String[] args) {
-        Stack<String> ops = new Stack<String>();
-        Queue<Double> qvals = new LinkedList<>();
-        System.out.println("Shunting Yard Algorithm 62050140 Chonnaphat Visetchock\n");
-        System.out.print("Enter infix : ");
+        Datafinal result = new Datafinal();
         Scanner input = new Scanner(System.in);
-        String[] tokens = input.nextLine().split(" ");
-        System.out.print("Infix : ");
-        for (int i = 0; i < tokens.length; i++)
-            System.out.print(tokens[i]);
-        System.out.println();
-        for (int i = 0; i < tokens.length; i++) {
-            if (tokens[i].equals("("))
-                ;
-            else if (tokens[i].equals("+"))
-                ops.push(tokens[i]);
-            else if (tokens[i].equals("-"))
-                ops.push(tokens[i]);
-            else if (tokens[i].equals("*"))
-                ops.push(tokens[i]);
-            else if (tokens[i].equals("/"))
-                ops.push(tokens[i]);
-            else if (tokens[i].equals(")")) {
-            } else
-                qvals.add(Double.parseDouble(tokens[i]));
+        try {
+            System.out.println();
+            System.out.println("Shunting Yard Algorithm 62050140 Chonnaphat Visetchock\n");
+            System.out.print("Enter infix : ");
+            String tokens = input.nextLine();
+            System.out.print("Infix : " + tokens);
+            System.out.println();
+            System.out.print("Postfix :");
+            System.out.println();
+            System.out.print("Stack Data : ");
+            System.out.println();
+            System.out.print("Queue Data : ");
+            System.out.println();
+            System.out.print("Result : " + result.evaluate(tokens));
+        } finally{
+            input.close();
         }
-        System.out.print("Postfix :");
-        System.out.println();
-        System.out.print("Stack Data : ");
-        PrintStack(ops);
-        System.out.println();
-        System.out.print("Queue Data : " + qvals);
-        System.out.println();
-        System.out.print("Result : ");
+
     }
 
 }
